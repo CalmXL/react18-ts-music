becf06b4bf13edb80ba76460b5152ce391e28b63
xl <xl18845898715@gmail.com>
2024-11-17 22:10:06 +0800

    feat: 路由懒加载 + suspense

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { lazy } from 'react'
 import { Navigate } from 'react-router-dom'
 import type { RouteObject } from 'react-router-dom'
 
-import Discover from '@/views/discover'
-import Mine from '@/views/mine'
-import Download from '@/views/download'
-import Focus from '@/views/focus'
+// import Discover from '@/views/discover'
+// import Mine from '@/views/mine'
+// import Download from '@/views/download'
+// import Focus from '@/views/focus'
+
+// 路由的懒加载
+const Discover = lazy(() => import('@/views/discover'))
+const Mine = lazy(() => import('@/views/mine'))
+const Download = lazy(() => import('@/views/download'))
+const Focus = lazy(() => import('@/views/focus'))
 
 const routes: RouteObject[] = [
   {
