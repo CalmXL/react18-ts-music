f5bb6b55b263b71332b399d34badf26585975b10
xl <xl18845898715@gmail.com>
2024-11-18 08:49:55 +0800

    feat: react-redux 使用 + 类型

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import React, { Suspense } from 'react'
 import { Link, useRoutes } from 'react-router-dom'
+import { useSelector, shallowEqual } from 'react-redux'
+
 import routes from './router'
+import store from './store'
+
+// type IRootState =
+type GetStateFnType = typeof store.getState
+type FnReturnType = ReturnType<GetStateFnType>
 
 function App() {
+  const { count, name } = useSelector(
+    (state: any) => ({
+      count: state.counter.count,
+      name: state.counter.name
+    }),
+    shallowEqual
+  )
+
   return (
     <div className="App">
       <div className="nav">
@@ -11,6 +26,9 @@ function App() {
         <Link to="/focus">关注</Link>
         <Link to="/download">下载客户端</Link>
       </div>
+      <h2>
+        当前计数: {count}-{name}
+      </h2>
       <Suspense fallback="">
         <div className="main">{useRoutes(routes)}</div>
       </Suspense>
