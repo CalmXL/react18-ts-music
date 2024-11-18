c731e535af262a555dab3a06e632d158285feaaf
xl <xl18845898715@gmail.com>
2024-11-18 08:20:41 +0800

    feat: 二级路由处理

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -12,6 +12,12 @@ const Discover = lazy(() => import('@/views/discover'))
 const Mine = lazy(() => import('@/views/mine'))
 const Download = lazy(() => import('@/views/download'))
 const Focus = lazy(() => import('@/views/focus'))
+const Recommend = lazy(() => import('@/views/discover/c-views/recommend'))
+const Rank = lazy(() => import('@/views/discover/c-views/rank'))
+const Songs = lazy(() => import('@/views/discover/c-views/songs'))
+const Djradio = lazy(() => import('@/views/discover/c-views/djradio'))
+const Artist = lazy(() => import('@/views/discover/c-views/artist'))
+const Album = lazy(() => import('@/views/discover/c-views/album'))
 
 const routes: RouteObject[] = [
   {
@@ -20,7 +26,37 @@ const routes: RouteObject[] = [
   },
   {
     path: '/discover',
-    element: <Discover />
+    element: <Discover />,
+    children: [
+      {
+        path: '/discover',
+        element: <Navigate to="/discover/recommend" />
+      },
+      {
+        path: '/discover/recommend',
+        element: <Recommend />
+      },
+      {
+        path: '/discover/rank',
+        element: <Rank />
+      },
+      {
+        path: '/discover/songs',
+        element: <Songs />
+      },
+      {
+        path: '/discover/djradio',
+        element: <Djradio />
+      },
+      {
+        path: '/discover/artist',
+        element: <Artist />
+      },
+      {
+        path: '/discover/album',
+        element: <Album />
+      }
+    ]
   },
   {
     path: '/mine',
