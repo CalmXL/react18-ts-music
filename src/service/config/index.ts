9cec231e6361c2580b52ecf4c37adad87a463913
xl <xl18845898715@gmail.com>
2024-11-21 08:27:50 +0800

    feat: 环境变量的区分以及类型定义

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,16 @@
 export const BASE_URL = 'http://123.207.32.32:9002/'
 export const TIME_OUT = 8000
+
+// 1. webpack 获取当前依赖当前环境 development/production
+// let BASE_URL = ''
+// console.log(process.env.NODE_ENV)
+// if (process.env.NODE_ENV === 'development') {
+//   BASE_URL = 'http://123.207.32.32:9002/'
+// } else {
+//   BASE_URL = 'http://123.207.32.32:9002/'
+// }
+
+// 2. 配置 .env.development/production 文件
+console.log(process.env.REACT_APP_BASE_URL)
+
+// export { BASE_URL }
