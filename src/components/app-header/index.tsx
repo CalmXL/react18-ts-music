b0defce0f588781da50bb5332227a3caab1bc995
xl <xl18845898715@gmail.com>
2024-11-25 08:21:35 +0800

    feat: header titles 基本渲染

--- src/components/app-header/index.tsx
+++ src/components/app-header/index.tsx
@@ -1,20 +1,43 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { Link } from 'react-router-dom'
-import { HeaderWrapper } from '../app-footer/style'
+import { HeaderLeft, HeaderRight, HeaderWrapper } from '../app-footer/style'
+import headerTitles from '@/assets/data/header-title.json'
 
 interface IProps {
   children?: ReactNode
 }
 
 const AppHeader: FC<IProps> = () => {
+  function showItem(item: any) {
+    if (item.type === 'path') {
+      return <Link to={item.link}>{item.title}</Link>
+    }
+    return (
+      <a href={item.link} target="_blank" rel="noreferrer">
+        {item.title}
+      </a>
+    )
+  }
+
   return (
     <HeaderWrapper>
-      <div className="content">
-        <Link to="/discover">发现音乐</Link>
-        <Link to="/mine">我的音乐</Link>
-        <Link to="/focus">关注</Link>
-        <Link to="/download">下载客户端</Link>
+      <div className="content wrap-v1">
+        <HeaderLeft>
+          <a className="logo sprite_01" href="/">
+            网易云音乐
+          </a>
+          <div className="title-list">
+            {headerTitles.map((item) => {
+              return (
+                <div className="item" key={item.title}>
+                  {showItem(item)}
+                </div>
+              )
+            })}
+          </div>
+        </HeaderLeft>
+        <HeaderRight>right</HeaderRight>
       </div>
     </HeaderWrapper>
   )
